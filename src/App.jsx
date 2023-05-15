6c88834741cd502d8a6e945ae5178949e5feffe2
Senan747 <111648854+Senan747@users.noreply.github.com>
2023-05-15 18:52:07 +0400

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import Pages from "./pages/Pages"
+import Pages from "./pages/Pages";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
       <Pages />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
